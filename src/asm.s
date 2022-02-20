50cbfd88ca7afb67de7d9bbc45a2ed0c83fe8d75
Cracked5pider <cracked5pider@protonmail.com>
2022-02-20 02:33:42 +0100

    fix space second time

--- src/asm.s
+++ src/asm.s
@@ -5,23 +5,23 @@ global Start
 global GetRIP
 
 section .text$A
-    Start:
-		    push  rsi
-		    mov	  rsi, rsp
-		    and	  rsp, 0FFFFFFFFFFFFFFF0h
+	Start:
+		push  	rsi
+		mov	  	rsi, rsp
+		and	  	rsp, 0FFFFFFFFFFFFFFF0h
 
-		    sub	  rsp, 020h
-		    call	Entry
+		sub	  	rsp, 020h
+		call	Entry
 
-		    mov		rsp, rsi
-		    pop		rsi
-	  ret
+		mov		rsp, rsi
+		pop		rsi
+	ret
 
 section .text$F
     GetRIP:
    	    call	retptr
 
     retptr:
-        pop	rax
-        sub	rax, 5
+        pop		rax
+        sub		rax, 5
     ret
